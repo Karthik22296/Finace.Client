6fdaed8fbc177e5da70515b56ef3f776393e58a6
karthik <karthik22296@gmail.com>
2026-09-14 16:23:51 +0530

    feat: add generated API configuration for Angular client

--- src/api/api-configuration.ts
+++ src/api/api-configuration.ts
@@ -23,7 +23,7 @@ export function provideApiConfiguration(rootUrl: string) {
 })
 export class ApiConfiguration {
   //  rootUrl: string = 'https://localhost:44378';
-  rootUrl: string = 'https://finace-aggbd3ebbvfwdee8.southindia-01.azurewebsites.net/';
+  rootUrl: string = 'https://finace-aggbd3ebbvfwdee8.southindia-01.azurewebsites.net';
 
 }
 
